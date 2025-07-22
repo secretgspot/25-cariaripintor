99f3bd06714bc7dcfc00f434874fdfd592ac89e2
Secret G Spot <secregspot@gmail.com>
2025-07-22 15:53:01 -0600

    devtools plugin so it stop yelling

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import { sveltekit } from '@sveltejs/kit/vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
+		devtoolsJson(),
 		paraglideVitePlugin({
 			project: './project.inlang',
 			outdir: './src/lib/paraglide'
